0f5b3d7fcb2b08a559352973a4b19d2802b7b670
rockycsumb <rocky@solidrockdesigns.io>
2023-11-29 12:12:13 -0800

    ini files

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import {Routes, Route} from 'react-router-dom'
+import {Home} from './_root/pages';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import './globals.css';
+import RootLayout from './_root/RootLayout';
+import AuthLayout from './_auth/AuthLayout';
+
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+    <Routes>
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-in" element={<SignupForm />} />
+      </Route>
+        
+      <Route element={<RootLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+    </main>
+  )
+}
+
+export default App
